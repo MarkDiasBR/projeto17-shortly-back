5df0e606943b39669d8a1cfb4bc0b95b235bbe24
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-05-19 16:39:06 -0300

    feat: implemented the routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+// import { schemaValidation } from '../middlewares/schema.middleware.js';
+// import { signupSchema, signinSchema } from '../schemas/user.schemas.js';
+// import signupConflictValidation from '../middlewares/signup.middleware.js';
+// import signinValidation from '../middlewares/signin.middleware.js';
+// import { signup, signin } from '../controllers/user.controller.js';
+
+
+
+const userRouter = Router();
+
+// userRouter.post('/sign-up', schemaValidation(signupSchema), signupConflictValidation, signup);
+// userRouter.post('/sign-in', schemaValidation(signinSchema), signinValidation, signin);
+userRouter.post('/signup', signup);
+userRouter.post('/signin', signin);
+userRouter.get('/urls/:id');
+userRouter.get('/urls/open/:shortUrl');
+userRouter.get('/ranking');
+
+export default userRouter;
